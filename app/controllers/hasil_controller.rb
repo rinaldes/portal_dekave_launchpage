01c320cc25d146918af485bedbdab3538ec5e08a
Rinal <rinaldes.duma@gmail.com>
2019-10-24 14:00:55 +0700

    19102401 - Laporan

--- app/controllers/hasil_controller.rb
+++ app/controllers/hasil_controller.rb
@@ -2,7 +2,18 @@ class HasilController < ApplicationController
     
     def index
         @dataHasil = Sub.all
-        byebug
+        @masalah1 = 0
+        @masalah2 = 0
+        @masalah3 = 0
+        @masalah4 = 0
+        @masalah5 = 0
+        @dataHasil.each do |hasildata|
+            @masalah1 += hasildata.masalah.split("")[0].to_i rescue 0
+            @masalah2 += hasildata.masalah.split("")[1].to_i rescue 0
+            @masalah3 += hasildata.masalah.split("")[2].to_i rescue 0
+            @masalah4 += hasildata.masalah.split("")[3].to_i rescue 0
+            @masalah5 += hasildata.masalah.split("")[4].to_i rescue 0
+        end
         respond_to do |format|
             format.html
         end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -19,3 +19,4 @@ import "@fortawesome/fontawesome-free/js/all";
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
