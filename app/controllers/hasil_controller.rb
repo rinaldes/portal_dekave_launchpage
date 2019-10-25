7f9d3dafffba7f041a87d1101ee459e5ac6269c0
Rinal <rinaldes.duma@gmail.com>
2019-10-25 14:23:22 +0700

    19102503 - Repair

--- app/controllers/hasil_controller.rb
+++ app/controllers/hasil_controller.rb
@@ -7,12 +7,14 @@ class HasilController < ApplicationController
         @masalah3 = 0
         @masalah4 = 0
         @masalah5 = 0
+        @masalah6 = 0
         @dataHasil.each do |hasildata|
             @masalah1 += hasildata.masalah.split("")[0].to_i rescue 0
             @masalah2 += hasildata.masalah.split("")[1].to_i rescue 0
             @masalah3 += hasildata.masalah.split("")[2].to_i rescue 0
             @masalah4 += hasildata.masalah.split("")[3].to_i rescue 0
             @masalah5 += hasildata.masalah.split("")[4].to_i rescue 0
+            @masalah5 += hasildata.masalah.split("")[5].to_i rescue 0
         end
         respond_to do |format|
             format.html
